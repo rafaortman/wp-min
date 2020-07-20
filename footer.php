725149cc326e674b340071c874c1b14a3146d511
Rafa Ortman <rafaortman@gmail.com>
2020-07-20 11:46:02 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
+<footer id="siteRodape">
+    <?php if ( is_user_logged_in() ): ?>
+        <a href="<?php echo admin_url(); ?>">
+            WP-admin
+        </a>
+    <?php endif; ?>
+</footer>
+
 <?php wp_footer(); ?>
-    <footer id="siteRodape">
-        <?php if ( is_user_logged_in() ): ?>
-            <a href="<?php echo admin_url(); ?>">
-                WP-admin
-            </a>
-        <?php endif; ?>
-    </footer>
+
 </body>
 </html>
