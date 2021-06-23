26966088cf69c1bf4d4200b92bd3b20a1ca3dd86
Rafa Ortman <rafaortman@gmail.com>
2021-06-23 20:29:51 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,24 @@
         return elementBottom > viewportTop && elementTop < viewportBottom;
     };
     
+    function string_to_slug(str) {
+        str = str.replace(/^\s+|\s+$/g, ''); // trim
+        str = str.toLowerCase();
+
+        // remove accents, swap ñ for n, etc
+        var from = "àáäâãèéëêìíïîòóöôõùúüûñç·/_,:;";
+        var to = "aaaaaeeeeiiiiooooouuuunc------";
+        for (var i = 0, l = from.length; i < l; i++) {
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+        }
+
+        str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+            .replace(/\s+/g, '-') // collapse whitespace and replace by -
+            .replace(/-+/g, '-'); // collapse dashes
+
+        return str;
+    }
+    
     $(window).on('resize scroll', function () {
         $('.section').each(function () {
             var sectionAtiva = $(this).attr('id');
@@ -78,7 +96,7 @@
         console.log('fechar');
     });
 
-    $('.fechar').on('click', function(){
+    $('.modal-fechar').on('click', function(){
         $('body').removeClass('modal-ativo');
         $(this).closest('.modal').removeClass('ativo');
     });
