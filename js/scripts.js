4101808714225339cc2ece4e62320d36a80b444d
Rafa Ortman <rafaortman@gmail.com>
2021-06-23 20:28:17 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
 (function ($) {
+    
+    $.fn.isInViewport = function () {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+    
+    $(window).on('resize scroll', function () {
+        $('.section').each(function () {
+            var sectionAtiva = $(this).attr('id');
+            var menuAtivo = '#siteTopo .menu a[href*="#' + sectionAtiva + '"]';
+            if ($(this).isInViewport()) {
+                $('#' + sectionAtiva).addClass('ativo');
+                $(menuAtivo).addClass('ativo').parent().siblings().find('a').removeClass('ativo');
+            } else {
+                $('#' + sectionAtiva).removeClass('ativo');
+            }
+            if($(window).scrollTop() < $('.section').first().offset().top - 100) {
+                $('.section').removeClass('ativo');
+                $('#siteTopo .menu a').removeClass('ativo');
+            }
+        });
+    });
 
     $('a[href*="#"]').click(function (event) {
         if (
@@ -28,6 +55,32 @@
         $(this).toggleClass('ativo');
         $('.menu').toggleClass('ativo');
         $('#searchForm').removeClass('ativo');
+    });
+    
+    $('.carrossel').slick({
+        autoplay: true,
+        autoplaySpeed: 6000,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false,
+        dots: true
+    });
+
+    $('.modal-trigger').on('click', function(){
+        $('body').addClass('modal-ativo');
+        $(this).find('.modal').addClass('ativo');
+    });
+
+    $('.modal').on('click', function(e){
+        e.stopPropagation();
+        $('body').removeClass('modal-ativo');
+        $(this).removeClass('ativo');
+        console.log('fechar');
+    });
+
+    $('.fechar').on('click', function(){
+        $('body').removeClass('modal-ativo');
+        $(this).closest('.modal').removeClass('ativo');
     });
     
 })(jQuery);
