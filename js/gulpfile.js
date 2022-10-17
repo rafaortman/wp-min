726033cb3ce4c869a424e4c33bc8e002022bcca6
Rafa Ortman <rafaortman@gmail.com>
2022-10-17 12:33:07 -0300

    Update gulpfile.js

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp"),
     sass = require("gulp-sass"),
+//  sass = require('gulp-sass')(require('sass')),
     postcss = require("gulp-postcss"),
     autoprefixer = require("autoprefixer"),
     cssnano = require("cssnano"),
